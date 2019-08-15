6179685e839d78068c7afab8d9b30821e4b7bae3
deviprajwala <deviprajwala3@gmail.com>
2019-08-15 11:30:36 +0530

    works perfectly

--- seating_arrangement.c
+++ seating_arrangement.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-void check(int a[6][8],int x);
+void seat(int i);
 int main()
 {
-	int i,j,x,y,k,a[6][8],p=0,m,n,count=1,x1,x2;
+	int i,j,d,x,y,k,a[6][8],p=0,m,n,count=1,e;
         long int c[48]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48};
+       
 	
 	label1:
 	{
@@ -36,30 +37,59 @@ int main()
           for(n=1;n<=8;n++)
           {
             printf("%d\t",a[m][n]);
+           
           }
+           if(m==3)
+            {
+             printf("\n\n");
+            }
           printf("\n");
        }
-printf("Enter two seat numbers\n");
-scanf("%d %d",&x1,&x2);
-check(a[6][8], x1);
-check(a[6][8], x2);
-
-return 0;
-}
-void check(int a[6][8],int x)
+e=1;
+while(e!=0)
 {
-int i,j,k;
-for(k=0;k<2;k++)
- {
-   for(i=1;i<=8;i++)
+   printf("Enter the seat number:");
+   scanf("%d",&d);
+   for(i=1;i<=6;i++)
    {
-    for(j=1;j<=6;j++)
+    for(j=1;j<=8;j++)
     {
-      if(a[i][j]==x)
+      if(a[i][j]==d)
       {
-       printf("%d",a[i][j+1]);
+       if(j%2==0)
+       {
+       printf("The seat opposite to %d is %d\n",d,a[i][j-1]);
+       seat(i);
+       printf("Enter 1 to continue and 0 to exit\n");
+       scanf("%d",&e);
+       }
+       else
+       {
+        printf("The seat opposite to %d is %d\n",d,a[i][j+1]);
+        seat(i);
+        printf("Enter 1 to continue and 0 to exit\n");
+        scanf("%d",&e); 
+       }
       }
     }
    }
- }
-}  
+} 
+  
+return 0;
+}
+void seat(int i)
+{      
+       if(i==1||i==6)
+       {
+       printf("Window seat\n");
+       }
+       else if(i==2||i==5)
+       {
+       printf("Middle seat\n");
+       }
+       else if(i==3||i==4)
+       {
+       printf("Asile seat seat\n");
+       }
+      
+}
